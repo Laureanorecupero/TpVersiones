0692e432f1ce73f39282a2a9d9115d379cfd4df6
Laureanorecupero <laureanorecupero@gmail.com>
2015-06-02 19:02:21 -0300

    Inicio

---
+++ Operaciones.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo
+{
+    public class Operaciones
+    {
+        float x { get; set; }
+
+        float y { get; set; }
+        public Operaciones()
+        {
+            try
+            {
+                x = float.Parse(Console.ReadLine());
+                y = float.Parse(Console.ReadLine());
+            }
+            catch (FormatException fe)
+            {
+                Console.WriteLine(fe.ToString());
+                x = 0;
+                y = 0;
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("ups" + e.Message);
+            }
+        }
+
+        public float suma()
+        {
+            return x + y;
+        }
+        public float resta()
+        {
+            return x - y;
+        }
+        public float producto()
+        {
+            return x * y;
+        }
+        public float division()
+        {
+            return x / y;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operaciones o = new Operaciones();
+            Console.WriteLine(o.suma());
+            Console.WriteLine(o.resta());
+            Console.WriteLine(o.producto());
+            Console.WriteLine(o.division());
+            Console.ReadKey();
+        }
+    }
+}
